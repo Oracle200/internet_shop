b4da5b0731ccb436764d40625ceae57f940089e6
Ilnur <sal73ilnur@gmail.com>
2022-11-12 00:16:53 +0400

    Создали новые шаблоны

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'IndexController')->name('index');
 Route::get('/catalog/index', 'CatalogController@index')->name('catalog.index');
 Route::get('/catalog/category/{slug}', 'CatalogController@category')->name('catalog.category');
 Route::get('/catalog/brand/{slug}', 'CatalogController@brand')->name('catalog.brand');
